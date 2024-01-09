4707eaf5e33758a320cc2d7e7102542c5cb812de
diegoteran99 <diegoteranbaselli@gmail.com>
2024-01-09 13:22:54 -0300

    model_u_net import fix

--- evaluation.py
+++ evaluation.py
@@ -26,7 +26,7 @@ from tqdm.autonotebook import tqdm
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import jaccard_score
-from model_u_net import DoubleConv, Down, Up, OutConv, UNet
+from model_u_net import DoubleConv, Down, Up, OutConv, UNet, model
 from parameters import LS_max_as, LI_min_as, mean_as, std_as, min_as, max_as, LS_max128, LI_min128, mean_128, std_128, min_128, max_128
 from train128 import create_dataset128
 from trainAS import create_datasetAS
